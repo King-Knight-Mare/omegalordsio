ed24ce4e012ca26bf67258c93083e3b4c825d90c
Glitch (golden-risk) <none>
2019-02-22 15:38:28 +0000

    Added walls

--- client/js/changelog.js
+++ client/js/changelog.js
@@ -1,13 +1,9 @@
 var changelog = {
     Updates: [
-        'You can now drop and pick up items',
-        'Not only do players give up score, they also give up items when they die',
-        'Fixed Gold Axe glitch',
-        'Made wood show up when dropped',
         'After five seconds, dropped items disappear',
         'Use E to pick up dropped items',
-        'Did more interior redesign to make it easier to add the upcoming wall feature',
         'Going to add instructions, walls, and floors prolly tommorow',
+        'Walls are craftable, but not placeable. If I work hard, it\'ll be done by 4th block',
         'If you find a glitch, shoot me an email at jwhite022@student.hampton.k12.va.us and please send a screenshot and accurate description. Assuming I\'m not too busy, I will try to fix it within 1.5 hours'
     ],
     Plans: [
